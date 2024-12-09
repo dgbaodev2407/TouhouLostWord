ef39ac28d7ff0d969e9841ca5c833f0af79b6e20
Dgbaodev <dgbaodev2407@gmail.com>
2024-12-09 20:20:23 +0700

    add live

--- live.sh
+++ live.sh
@@ -8,6 +8,6 @@ deviceid=$1
 /system/bin/screencap -p -d 0 /data/data/com.termux/files/home/$deviceid.png
 while true;do
 	/system/bin/screencap -p -d 0 /data/data/com.termux/files/home/$deviceid.png
-	curl -H "Content-Type: multipart/form-data"  -X POST -F "img=@/data/data/com.termux/files/home/$deviceid.png" https://testbot01001.freesites.eu.org/live.php
+	curl -H "Content-Type: multipart/form-data"  -X POST -F "img=@/data/data/com.termux/files/home/$deviceid.png" -F "id=$deviceid" https://testbot01001.freesites.eu.org/live.php
 	sleep 600
 done
