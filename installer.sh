ac5a965e88bb49e18ed848f5729915bc88d96a91
Dgbaodev <dgbaodev2407@gmail.com>
2024-12-09 09:27:35 +0700

    Fix

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pkg install -y git
-git clone https://github.com/dgbaodev2407/TouhouLostWord
+# pkg install -y git
+cd  /data/data/com.termux/files/home/TouhouLostWord && git clone https://github.com/dgbaodev2407/TouhouLostWord
 wget https://github.com/dgbaodev2407/TouhouLostWord/releases/download/Packages/TouhouLostWordJP.apks -O /data/data/com.termux/files/home/TouhouLostWord/TouhouLostWordJP.apks
 settings put global package_verifier_enable 0
 pm install /data/data/com.termux/files/home/TouhouLostWord/GameTool.apk
